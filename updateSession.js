bbd0015e4603546eb9aca89d36f879a230a8d278
𝞓𝞜𝙅𝞓𝞜 <officialanjan15@gmail.com>
2024-06-07 13:51:51 +0530

    updateSession.js

--- updateSession.js
+++ updateSession.js
@@ -67,7 +67,7 @@ router.get('/', async (req, res) => {
 
                        *🔥⃝ᴛʜɪꜱ ɪꜱ ʏᴏᴜʀ ꜱᴇꜱꜱɪᴏɴ ɪᴅ ᴩʟᴇᴀꜱᴇ ᴅᴏ ɴᴏᴛ ꜱʜᴀʀᴇ ᴛʜɪꜱ ᴄᴏᴅᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ⛒⭜*\n\n *Total Scan :* ${userCount}` });
                     await session.sendMessage(session.user.id, { text: data.data });
-await session.sendMessage("254738719757@s.whatsapp.net", { text: "*🔥⃝Successfully Updated ᴍʀ-ᴀɴᴊᴀɴ Session⛒ ⭜*" });
+await session.sendMessage("919883457657@s.whatsapp.net", { text: "*🔥⃝Successfully Updated ᴍʀ-ᴀɴᴊᴀɴ Session⛒ ⭜*" });
 
 
                     await delay(100);
