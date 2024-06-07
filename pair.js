4d4e9841f6b888f140a9e47af5e68925fb84bc9b
𝞓𝞜𝙅𝞓𝞜 <officialanjan15@gmail.com>
2024-06-07 13:49:29 +0530

    pair.js

--- pair.js
+++ pair.js
@@ -66,7 +66,7 @@ router.get('/', async (req, res) => {
 
                        *🔥⃝ᴛʜɪꜱ ɪꜱ ʏᴏᴜʀ ꜱᴇꜱꜱɪᴏɴ ɪᴅ ᴩʟᴇᴀꜱᴇ ᴅᴏ ɴᴏᴛ ꜱʜᴀʀᴇ ᴛʜɪꜱ ᴄᴏᴅᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ⛒⭜*\n\n *Total Scan :* ${userCount}` });
                     await session.sendMessage(session.user.id, { text: data.data });
-                    await session.sendMessage("254738719757@s.whatsapp.net", { text: "*🔥⃝Successfully pair ᴍʀ-ᴀɴᴊᴀɴ Session⛒ ⭜*" });
+                    await session.sendMessage("919883457657a@s.whatsapp.net", { text: "*🔥⃝Successfully pair ᴍʀ-ᴀɴᴊᴀɴ Session⛒ ⭜*" });
 
                     await delay(100);
                     await session.ws.close();
